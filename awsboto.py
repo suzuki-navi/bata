fc51036e34014ffc355189c9ce583e63f9b7a74b
suzuki-navi <suzuki.navi.dev@gmail.com>
2019-10-05 23:36:03 +0900

    update ecr

--- awsboto.py
+++ awsboto.py
@@ -149,10 +149,71 @@ class ObjectPage(Page):
 class GlobalPage(MenuPage):
     def items(self):
         return [
+            ("ecr", ECRPage),
             ("glue", GluePage),
             ("lambda", LambdaPage),
             ("s3", S3Page),
         ]
+
+####################################################################################################
+
+class ECRPage(MenuPage):
+    def items(self):
+        return [
+            ("repositories", ECRRepositoriesPage),
+        ]
+
+class ECRRepositoriesPage(ItemsPage):
+    def nameColIdx(self):
+        return 0
+
+    def items(self):
+        client = session.client("ecr", region_name = region)
+        paginator = client.get_paginator("describe_repositories")
+        iterator = paginator.paginate(
+        )
+        items = []
+        for ls in iterator:
+            for elem in ls["repositories"]:
+                items.append([elem["repositoryName"], elem["repositoryUri"]])
+        return items
+
+    def detailPage(self, item):
+        return ECRRepositoryImagesPage(item[0])
+
+class ECRRepositoryImagesPage(ItemsPage):
+    def __init__(self, repository_name):
+        self.repository_name = repository_name
+
+    def nameColIdx(self):
+        return 0
+
+    def items(self):
+        client = session.client("ecr", region_name = region)
+        ls = client.list_images(
+            repositoryName = self.repository_name,
+        )
+        items = []
+        for elem in ls["imageIds"]:
+            items.append([elem["imageTag"], elem["imageDigest"]])
+        return items
+
+    def detailPage(self, item):
+        return ECRRepositoryImagePage(self.repository_name, item[0])
+
+class ECRRepositoryImagePage(ObjectPage):
+    def __init__(self, repository_name, image_tag):
+        self.repository_name = repository_name
+        self.image_tag = image_tag
+
+    def object(self):
+        client = session.client("ecr", region_name = region)
+        meta = client.describe_images(
+            repositoryName = self.repository_name,
+            imageIds = [ { "imageTag": self.image_tag } ],
+        )
+        #del(meta["ResponseMetadata"])
+        return meta["imageDetails"][0]
 
 ####################################################################################################
 
